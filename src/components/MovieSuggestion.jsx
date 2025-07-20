23478facd1232036f24c6ca500040148b5138219
Yash Pandey <yashpandey1907@gmail.com>
2025-07-20 21:54:03 +0530

    changed a little in ui increase the distance between cards

--- src/components/MovieSuggestion.jsx
+++ src/components/MovieSuggestion.jsx
@@ -6,14 +6,14 @@ function MovieSuggestion() {
   const { movieData: movie } = useSelector((state) => state.movieData);
 
   return (
-    <div className="flex flex-wrap justify-center gap-4 px-4 mt-5">
+    <div className="flex flex-wrap justify-center gap-3 px-4 mt-5">
       {!movie ? (
         <div className="flex w-screen h-screen justify-center items-center">
           <p className="text-white text-center mt-10 text-xl">Loading...</p>
         </div>
       ) : movie?.length > 0 ? (
         movie.map((mov, idx) => (
-          <div key={idx} className="w-[200px]">
+          <div key={idx} className="w-[230px]">
             <MovieCard movie={mov} />
           </div>
         ))
